4f341ae9735ab9549315a760319f9443c830825c
Sheldon <alphasxd@outlook.com>
2023-08-03 13:48:01 +0800

    feat: subarry sum

---
+++ 560/code.go
@@ -0,0 +1,28 @@
+// 560. 和为 K 的子数组 https://leetcode-cn.com/problems/subarray-sum-equals-k/
+
+// 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+
+// 示例 1：
+// 输入：nums = [1,1,1], k = 2
+// 输出：2
+
+// 示例 2：
+// 输入：nums = [1,2,3], k = 3
+// 输出：2
+
+package leetcode
+
+func subarraySum(nums []int, k int) int {
+	cnt, preSum := 0, 0
+	// 定义一个map，key为前缀和，value为前缀和出现的次数
+	m := make(map[int]int)
+	m[0] = 1 // 前缀和初始化
+	for i := 0; i < len(nums); i++ {
+		preSum += nums[i]
+		if _, ok := m[preSum-k]; ok {
+			cnt += m[preSum-k] // 将前缀和为preSum-k的个数，加到cnt中
+		}
+		m[preSum]++ // 更新前缀和个数
+	}
+	return cnt
+}
---
+++ 560/code_test.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "testing"
+
+func TestSubarrySum(t *testing.T) {
+	var tests = []struct {
+		nums []int
+		k    int
+		want int
+	}{
+		{[]int{1, 1, 1}, 2, 2},
+		{[]int{1, 2, 3}, 3, 2},
+	}
+
+	for _, tt := range tests {
+		if want := subarraySum(tt.nums, tt.k); want != tt.want {
+			t.Errorf("subarraySum(%v, %v) return %v, want %v", tt.nums, tt.k, want, tt.want)
+		}
+	}
+}
