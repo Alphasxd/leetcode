c866a06df646606a3aa437c99f601617b288bfcb
Sheldon <alphasxd@outlook.com>
2023-08-13 12:53:59 +0800

    feat: length of longest substring

--- 003/code.go
+++ 003/code.go
@@ -1,5 +1,7 @@
 // 3. 无重复字符的最长子串 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
 // 示例 1：
 // 输入：s = "abcabcbb"
 // 输出：3
