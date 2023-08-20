5ea6872710a5444622375cb4d4e362d7ddfd180a
Sheldon <alphasxd@outlook.com>
2023-08-20 10:24:49 +0800

    feat: merge k lists

---
+++ 023/code.go
@@ -0,0 +1,74 @@
+// 23. 合并 K 个升序链表 https://leetcode.cn/problems/merge-k-sorted-lists/
+
+// 给你一个链表数组，每个链表都已经按升序排列。
+// 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
+package leetcode
+
+import "container/heap"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type ListNodeHeap []*ListNode
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	hp := ListNodeHeap{}
+	// 将每个待合并的链表的首元节点添加到小顶堆中
+	for _, head := range lists {
+		if head != nil {
+			hp = append(hp, head)
+		}
+	}
+	// 将 hp 初始化为堆
+	heap.Init(&hp)
+
+	// 创建哑节点，用于返回合并后的链表
+	dummy := &ListNode{}
+	curr := dummy
+	for hp.Len() > 0 {
+		// 取出堆顶元素（最小值）
+		min := heap.Pop(&hp).(*ListNode)
+		// min := hp.Pop().(*ListNode)
+		// 如果最小值存在后继节点，则将后继节点添加到堆中
+		if min.Next != nil {
+			// heap.Push(&hp, min.Next)
+			hp.Push(min.Next)
+		}
+
+		// 将最小值添加到合并后的链表中
+		curr.Next = min
+		curr = curr.Next
+		
+	}
+
+	return dummy.Next
+
+}
+
+// 实现最小堆
+func (h ListNodeHeap) Less(i, j int) bool {
+	return h[i].Val < h[j].Val
+}
+
+func (h ListNodeHeap) Len() int {
+	return len(h)
+}
+
+func (h *ListNodeHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func (h *ListNodeHeap) Push(x any) {
+	*h = append(*h, x.(*ListNode))
+}
+
+func (h ListNodeHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
