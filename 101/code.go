61931baaf33f7abc537261279d8c6d14421ef79e
Sheldon <alphasxd@outlook.com>
2023-08-23 12:48:50 +0800

    var function

--- 101/code.go
+++ 101/code.go
@@ -13,22 +13,22 @@
 package leetcode
 
 type TreeNode struct {
-	Val int
-	Left *TreeNode
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
 // 递归 时间复杂度O(n) 空间复杂度O(n)
 func isSymmetric(root *TreeNode) bool {
+	var check func(*TreeNode, *TreeNode) bool
+	check = func(left, right *TreeNode) bool {
+		if left == nil && right == nil {
+			return true
+		}
+		if left == nil || right == nil || left.Val != right.Val {
+			return false
+		}
+		return check(left.Left, right.Right) && check(left.Right, right.Left)
+	}
 	return check(root, root)
-}
-
-func check(left, right *TreeNode) bool {
-	if left == nil && right == nil {
-		return true
-	}
-	if left == nil || right == nil || left.Val != right.Val {
-		return false
-	}
-	return check(left.Left, right.Right) && check(left.Right, right.Left)
 }
