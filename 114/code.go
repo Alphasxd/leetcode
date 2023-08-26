b14b07ec5938f97bea74b9f36f7befa573420069
Sheldon <alphasxd@outlook.com>
2023-08-26 11:57:17 +0800

    feat: flatten

---
+++ 114/code.go
@@ -0,0 +1,40 @@
+// 114. 二叉树展开为链表 https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+
+// 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+// 展开后的单链表应该同样使用 TreeNode，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+// 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+
+// 示例 1：
+// 输入：root = [1,2,5,3,4,null,6]
+//           1
+//         /   \
+//        2     5
+//       / \     \
+//      3   4     6
+// 输出：[1,null,2,null,3,null,4,null,5,null,6]
+
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func flatten(root *TreeNode) {
+	// 从根节点开始，将左子树插入到右子树的地方
+	for ; root != nil; root = root.Right {
+		// 记录右子树
+		right := root.Right
+		// 将左子树插入到右子树的地方，原来的左子树置空
+		root.Left, root.Right = nil, root.Left
+
+		// 从root的右子树开始，找到最右边的节点
+		prev := root
+		for prev.Right != nil {
+			prev = prev.Right
+		}
+		// 将原来的右子树接到当前右子树的最右边节点
+		prev.Right = right
+	}
+}
