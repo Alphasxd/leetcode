027ddda503de8ec486b69bf3da3373c15237619c
Sheldon <alphasxd@outlook.com>
2023-12-05 12:17:10 +0800

    Add solution for validating palindrome strings

---
+++ double-pointer/125/code.go
@@ -0,0 +1,43 @@
+// 125. 验证回文串 https://leetcode-cn.com/problems/valid-palindrome/
+
+// 给你一个字符串 s ，验证 s 是否是 回文串 ，只考虑字母和数字字符，可以忽略字母的大小写。
+// 是返回 true ，否则返回 false 。
+
+package leetcode
+
+import "unicode"
+
+func isPalindrome(s string) bool {
+	// 双指针
+	i, j := 0, len(s)-1
+	
+	for i < j {
+		// 非字母数字，跳过
+		if !isAlphanumeric(rune(s[i])) {
+			i++
+			continue
+		}
+		if !isAlphanumeric(rune(s[j])) {
+			j--
+			continue
+		}
+		// 不相等，返回 false
+		if toLower(s[i]) != toLower(s[j]) {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func isAlphanumeric(c rune) bool {
+	return unicode.IsLetter(c) || unicode.IsDigit(c)
+}
+
+func toLower(c byte) byte {
+	if c >= 'A' && c <= 'Z' {
+		return c + 32
+	}
+	return c
+}
