2d077329b5d3bf8b43a695d5ff87d576c6f5cedd
Sheldon <alphasxd@outlook.com>
2023-12-27 15:22:59 +0800

    Refactor MinStack struct and methods

--- stack/155/code.go
+++ stack/155/code.go
@@ -7,14 +7,14 @@ package leetcode
 import "math"
 
 type MinStack struct {
-	stack []int
+	stack    []int
 	minstack []int // 辅助存储最小元素的栈
 }
 
 // 初始化堆栈对象
 func Constructor() MinStack {
 	return MinStack{
-		stack: []int{},
+		stack:    []int{},
 		minstack: []int{math.MaxInt64}, // 初始化时将最大值赋值给辅助栈
 	}
 }
@@ -22,14 +22,14 @@ func Constructor() MinStack {
 // 将元素 val 推入堆栈
 // 当一个元素要入栈时，首先取当前辅助栈的栈顶存储的最小值，然后与当前元素比较出最小值
 // 将得出的最小值推入辅助栈的栈顶，即辅助栈的栈顶存储当前栈所对应的最小值
-func (this *MinStack) Push(val int)  {
+func (this *MinStack) Push(val int) {
 	this.stack = append(this.stack, val)
 	top := this.minstack[len(this.minstack)-1]
 	this.minstack = append(this.minstack, min(val, top))
 }
 
 // 删除堆栈顶部的元素，将辅助栈的栈顶元素一同弹出
-func (this *MinStack) Pop()  {
+func (this *MinStack) Pop() {
 	this.stack = this.stack[:len(this.stack)-1]
 	this.minstack = this.minstack[:len(this.minstack)-1]
 }
