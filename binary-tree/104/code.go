c49126afa5273d30e158cb7a286fc5a9411349eb
Sheldon <alphasxd@outlook.com>
2024-01-02 21:22:51 +0800

    Refactor maxDepth function to use max instead of maxValue

--- binary-tree/104/code.go
+++ binary-tree/104/code.go
@@ -23,7 +23,5 @@ func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	return maxValue(maxDepth(root.Left), maxDepth(root.Right)) + 1
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
 }
-
-func maxValue(a, b int) int { if a > b { return a }; return b }
