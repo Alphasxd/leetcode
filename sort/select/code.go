394b545af562ea1f6b6ab9f40a103292dff90996
Sheldon <alphasxd@outlook.com>
2024-01-08 22:04:02 +0800

    Add selectSort function and test cases for sorting an array using selection sort algorithm

---
+++ sort/select/code.go
@@ -0,0 +1,15 @@
+package leetcode
+
+func selectSort(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		min := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[min] {
+				min = j
+			}
+		}
+		if min != i {
+			nums[i], nums[min] = nums[min], nums[i]
+		}
+	}
+}
