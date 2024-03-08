88d52b08e25536c7969b99e7f0a272e48605bda7
Sheldon <alphasxd@outlook.com>
2024-03-08 11:02:27 +0800

    Add variable names for clarity in climbStairs function

--- dynamic-program/070/code.go
+++ dynamic-program/070/code.go
@@ -16,6 +16,7 @@ package leetcode
 // 思路：动态规划，dp[i] = dp[i-1] + dp[i-2]，dp[i] 表示爬到第 i 阶的方法数
 // 爬到第i阶的方法数 = 爬到第i-1阶的方法数 + 爬到第i-2阶的方法数，因为每次可以爬1或2个台阶
 func climbStairs(n int) int {
+	// p:dp[i-2]，q:dp[i-1]，r:dp[i]
 	p, q, r := 0, 0, 1
 	for i := 1; i <= n; i++ {
 		p = q
