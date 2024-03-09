78a0a4ffa247103cc8dfad7b845bf37fcc00adf5
Sheldon <alphasxd@outlook.com>
2024-03-09 11:23:41 +0800

    Update LRUCache Notation

--- linked-list/146/code.go
+++ linked-list/146/code.go
@@ -47,15 +47,20 @@ func (c *LRUCache) Put(key int, value int) {
 	// 如果 key 存在，更新节点的值，并将节点移到链表头部
 	if elem, ok := c.cache[key]; ok {
 		c.list.MoveToFront(elem)
+		// 更新对应 key 的 value
 		elem.Value = pair{key, value}
 		return
 	}
+
+	// 添加新节点之前，需要判断缓存是否已满
 	// 如果 cache 已满，移除链表尾部节点，并删除哈希表中对应的项以及双向链表中的节点
 	// 删除链表尾部节点就符合了 LRU 的要求，因为尾部节点是最久未使用的，每次插入新节点或者更新节点都是在链表头部
 	if c.list.Len() == c.capacity {
-		delete(c.cache, c.list.Back().Value.(pair).key)
-		c.list.Remove(c.list.Back())
+		last := c.list.Back()
+		delete(c.cache, last.Value.(pair).key)
+		c.list.Remove(last)
 	}
 	// 在链表头部插入新节点，并在哈希表中添加 key 和节点的映射
+	// PushFront 返回的是 *list.Element，所以可以直接赋值给 map，同时完成了两个操作
 	c.cache[key] = c.list.PushFront(pair{key, value})
 }
