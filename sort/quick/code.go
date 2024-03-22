8d845d35d1eb01ec7720a8aa5f34b2ea01b21fea
Sheldon <alphasxd@outlook.com>
2024-03-22 20:51:57 +0800

    Refactor quickSort function and add comments

--- sort/quick/code.go
+++ sort/quick/code.go
@@ -2,27 +2,38 @@ package leetcode
 
 func quickSort(nums []int, lo, hi int) {
 	if lo < hi {
+		// 通过partition函数找到枢轴的位置
 		pivot := partition(nums, lo, hi)
+		// 对枢轴左侧的子区间进行快速排序
 		quickSort(nums, lo, pivot-1)
+		// 对枢轴右侧的子区间进行快速排序
 		quickSort(nums, pivot+1, hi)
 	}
 }
 
-// 一次划分，枢轴的位置是确定的，后续不再变化
-// 枢轴左边的元素都小于枢轴，右边的元素都大于枢轴，但左右两边的元素不一定有序
+// partition函数用于对数组nums的子区间[lo, hi]进行一次划分
+// 返回值是枢轴的最终位置
 func partition(nums []int, lo, hi int) int {
+	// 选择nums[lo]作为枢轴
 	pivot := nums[lo]
 	i, j := lo, hi
+	// 循环直到i和j相遇
 	for i < j {
+		// 从右向左找到第一个小于枢轴的元素
 		for i < j && nums[j] >= pivot {
 			j--
 		}
+		// 将这个元素移动到左侧
 		nums[i] = nums[j]
+		// 从左向右找到第一个大于枢轴的元素
 		for i < j && nums[i] <= pivot {
 			i++
 		}
+		// 将这个元素移动到右侧
 		nums[j] = nums[i]
 	}
+	// 将枢轴放到最终位置
 	nums[i] = pivot
+	// 返回枢轴的位置
 	return i
 }
