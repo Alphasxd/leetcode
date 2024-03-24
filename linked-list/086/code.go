905b7cc07eca215f41cf3e752aa4f1c434509de0
Sheldon <alphasxd@outlook.com>
2024-03-24 15:23:14 +0800

    Fix typo in comment

--- linked-list/086/code.go
+++ linked-list/086/code.go
@@ -1,6 +1,6 @@
 // 86. 分隔链表 https://leetcode-cn.com/problems/partition-list/
 
-// 给你一个链表的头节点 head 和yi一个特定值 x ，请你对链表进行分隔，
+// 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，
 // 使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。保留原始相对位置。
 
 package leetcode
