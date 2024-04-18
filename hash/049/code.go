dbbf472df7f4f12aa68bc0215c573dbe04e5eb7a
Sheldon <alphasxd@outlook.com>
2024-04-18 19:09:53 +0800

    Refactor groupAnagrams function to sort strings in alphabetical order

--- hash/049/code.go
+++ hash/049/code.go
@@ -21,6 +21,7 @@ func groupAnagrams(strs []string) [][]string {
 		// 将字符串转换为字节切片，因为字符串是不可变的，而字节切片是可变的
 		// 字符串是一个只读的 byte 类型切片
 		s := []byte(str)
+		// 对字节切片进行排序, 按字母顺序排序
 		sort.Slice(s, func(i, j int) bool {
 			return s[i] < s[j]
 		})
@@ -29,7 +30,6 @@ func groupAnagrams(strs []string) [][]string {
 	}
 
 	// 将 map 中的值转换为二维切片，因为 map 是无序的，所以需要遍历 map
-	// 使用 make 创建一个二维切片，指定长度和容量，长度为 0，容量为 map 的长度
 	ans := make([][]string, 0, len(mp))
 	for _, v := range mp {
 		ans = append(ans, v)
