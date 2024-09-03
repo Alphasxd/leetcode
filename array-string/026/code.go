29fd49386ebc75574dac16db75abe4aa2b7d8456
Sheldon <alphasxd@outlook.com>
2024-09-03 14:46:27 +0800

    update solution

--- array-string/026/code.go
+++ array-string/026/code.go
@@ -13,18 +13,17 @@
 package leetcode
 
 func removeDuplicates(nums []int) int {
+    n := len(nums)
     // nums 元素个数小于 2 时，直接返回原数组长度
-    if len(nums) < 2 {
-        return len(nums)
+    if n < 2 {
+        return n
     }
-    slow, fast := 0, 1 // 快慢指针
-    for fast < len(nums) {
-        if nums[slow] != nums[fast] {
-            slow++
+    slow := 1// 快慢指针
+    for fast := 1; fast < n; fast++ {
+        if nums[slow-1] != nums[fast] {
             nums[slow] = nums[fast]
+            slow++
         }
-        fast++
     }
-    // slow 为下标，长度为下标 + 1
-    return slow + 1
+    return slow
 }
