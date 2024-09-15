c25bba86038d5492fc6ac347ecc7ea8fce1166cd
Sheldon <alphasxd@outlook.com>
2024-09-15 14:37:47 +0800

    Optimize the algo

--- array-string/088/code.go
+++ array-string/088/code.go
@@ -10,13 +10,17 @@
 
 package leetcode
 
-import "sort"
-
-// 首先合并，然后排序
+// 时间复杂度：O(m+n)，空间复杂度：O(1)
 func merge(nums1 []int, m int, nums2 []int, n int) {
-    // 将 nums2 合并到 nums1 中的后 n 个位置
-    // nums1[m:] 表示 nums1 中从 m 开始的所有元素，切片操作是左闭右开的
-    copy(nums1[m:], nums2)
-    // 调用 sort 包中的排序方法，底层是快速排序和插入排序的结合
-    sort.Ints(nums1)
+    p1, p2, p := m-1, n-1, m+n-1
+    for p2 >= 0 { // nums2 还有元素未处理
+        if p1 >= 0 && nums1[p1] > nums2[p2] {
+            nums1[p] = nums1[p1]
+            p1--
+        } else {
+            nums1[p] = nums2[p2]
+            p2--
+        }
+        p--
+    }
 }
