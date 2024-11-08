f07dd861a85d7bdf57e58bbdb1f60fd84b42185b
Sheldon <alphasxd@outlook.com>
2024-11-08 11:30:58 +0800

    update solution for 88

--- array-string/088/code.go
+++ array-string/088/code.go
@@ -11,16 +11,18 @@
 package leetcode
 
 // 时间复杂度：O(m+n)，空间复杂度：O(1)
+// 比较 nums1[i] 和 nums2[j]，将较大的元素放在 nums1[k]，然后移动指针。
+// 重复此过程直到 nums2 的所有元素被合并。
 func merge(nums1 []int, m int, nums2 []int, n int) {
-    p1, p2, p := m-1, n-1, m+n-1
-    for p2 >= 0 { // nums2 还有元素未处理
-        if p1 >= 0 && nums1[p1] > nums2[p2] {
-            nums1[p] = nums1[p1]
-            p1--
+    i, j, k := m-1, n-1, m+n-1
+    for j >= 0 {
+        if i >= 0 && nums1[i] > nums2[j] {
+            nums1[k] = nums1[i]
+            i--
         } else {
-            nums1[p] = nums2[p2]
-            p2--
+            nums1[k] = nums2[j]
+            j--
         }
-        p--
+        k--
     }
 }
